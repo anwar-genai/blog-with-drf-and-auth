019bb747a3ca060811f1063736b540077fd79039
Anwar Khan <aws.cloud.anwar@gmail.com>
2025-10-02 04:32:57 -0700

    follow and unfollow feature added

--- follows/urls.py
+++ follows/urls.py
@@ -5,6 +5,8 @@ app_name = 'follows'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('people/', views.people, name='people'),
+    path('toggle/<int:user_id>/', views.toggle_follow, name='toggle'),
 ]
 
 
--- follows/views.py
+++ follows/views.py
@@ -1,6 +1,37 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Follow
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'follows/index.html')
+
+
+@login_required
+def people(request: HttpRequest) -> HttpResponse:
+    q = request.GET.get('q', '').strip()
+    users = User.objects.exclude(pk=request.user.pk)
+    if q:
+        users = users.filter(username__icontains=q)
+    following_ids = set(Follow.objects.filter(follower=request.user).values_list('following_id', flat=True))
+    return render(request, 'follows/people.html', { 'users': users, 'following_ids': following_ids, 'q': q })
+
+
+@login_required
+def toggle_follow(request: HttpRequest, user_id: int) -> HttpResponse:
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    target = get_object_or_404(User, pk=user_id)
+    if target == request.user:
+        messages.error(request, 'You cannot follow yourself.')
+        return redirect('follows:people')
+    rel, created = Follow.objects.get_or_create(follower=request.user, following=target)
+    if not created:
+        rel.delete()
+        messages.info(request, f'Unfollowed {target.username}.')
+    else:
+        messages.success(request, f'Now following {target.username}.')
+    return redirect('follows:people')
